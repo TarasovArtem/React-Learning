723efaf232345c664498fd5267af99b7d332292c
TarasovArtem <Hans8883@yandex.ru>
2021-01-31 17:09:50 +0100

    create components

---
+++ first-react-app/src/components/Tasks.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+
+
+const task = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 1st at 12:30pm', 
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'Doctors Appointment',
+        day: 'Feb 2nd at 13:30pm', 
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: 'Doctors Appointment',
+        day: 'Feb 3rd at 16:00pm', 
+        reminder: false,
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
